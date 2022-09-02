0f5227759eddaee318f6256687ce5da9986122aa
mbjn <mbjn@itu.dk>
2022-09-02 14:44:36 +0200

    .

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 static void main(string[] args) {
+    var hw = new HelloWorld();
 }
 public class HelloWorld {
     public String run() {
