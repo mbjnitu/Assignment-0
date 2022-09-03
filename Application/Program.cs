dbd4f3543145f2fa316cffd3818fdbf7622272bb
mbjn <mbjn@itu.dk>
2022-09-03 15:14:04 +0200

    Created user interface

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,7 +1,13 @@
 ﻿public class Program {
 
 static void Main(string[] args) {
-    var hw = new HelloWorld();
+
+    var arg = Console.ReadLine();
+    var ly = new LeapYear();
+
+    var res = ly.IsLeapYear(int.Parse(arg));
+    var response = res == true ? "yay" : "nay";
+    Console.WriteLine(response);
 }
 public class HelloWorld {
     public String run() {
@@ -9,4 +15,3 @@ public class HelloWorld {
     }
 }
 }
-//nothing
---
+++ ApplicationTest/LeapYearFromConsoleTest.cs
@@ -0,0 +1,22 @@
+namespace ApplicationTest;
+
+public class LeapYearFromConsoleTest
+{
+    [Fact]
+    public void CheckLeapYear()
+    {
+        // Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
+        using var reader = new StringReader("2000");
+        Console.SetIn(reader);
+
+        // Act
+        // No idea how to run the program from a test, giving it 2000, and checking if i recieve yay.
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("yay");
+    }
+}
